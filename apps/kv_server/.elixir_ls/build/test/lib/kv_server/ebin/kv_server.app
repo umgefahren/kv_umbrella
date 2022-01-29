e0557606caa912c1fb797c1dd4eb775ba9b4e66d
Hannes <55623006+umgefahren@users.noreply.github.com>
2022-01-29 17:19:46 +0100

    Initial Commit

---
+++ apps/kv_server/.elixir_ls/build/test/lib/kv_server/ebin/kv_server.app
@@ -0,0 +1,7 @@
+{application,kv_server,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"kv_server"},
+              {modules,['Elixir.KVServer','Elixir.KVServer.Application']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {mod,{'Elixir.KVServer.Application',[]}}]}.
---
+++ apps/kv_server/lib/kv_server.ex
@@ -0,0 +1,77 @@
+defmodule KVServer do
+  require Logger
+
+  @moduledoc """
+  Documentation for `KVServer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KVServer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
+
+  def loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    msg = 
+      case read_line(socket) do
+        {:ok, data} ->
+          IO.puts(data)
+          case KVServer.Command.parse(data) do
+            {:ok, command} ->
+              KVServer.Command.run(command)
+            {:error, _} = err ->
+              err
+          end
+        {:error, _} = err ->
+          err
+      end
+
+    write_line(socket, msg)
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, {:ok, text}) do
+    :gen_tcp.send(socket, text)
+  end
+
+  defp write_line(socket, {:error, :unknown_command}) do
+    :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
+  end
+
+  defp write_line(_socket, {:error, :closed}) do
+    exit(:shutdown)
+  end
+
+  defp write_line(socket, {:error, :not_found}) do
+    :gen_tcp.send(socket, "NOT FOUND\r\n")
+  end
+
+  defp write_line(socket, {:error, error}) do
+    :gen_tcp.send(socket, "ERROR\r\n")
+    exit(error)
+  end
+end
